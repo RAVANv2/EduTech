f7925d7ce6831d887c4e451d3272ac18a1991a89
satya9500 <satya.prakash9500@gmail.com>
2020-01-27 22:32:51 +0530

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.camera);
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI
+    });
+
+    function onSuccess(imageURI) {
+        var image = document.getElementById('myImage');
+        image.src = imageURI;
+
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
